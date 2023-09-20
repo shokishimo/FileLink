b9955d3326cd3622f9efd0eb2472763baa5eab6d
shokishimo <shokishimo@gmail.com>
2023-09-20 05:55:05 -0500

    init cdk

---
+++ test/file_link.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FileLink from '../lib/file_link-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/file_link-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FileLink.FileLinkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
