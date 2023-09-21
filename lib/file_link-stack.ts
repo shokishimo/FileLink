9af9bce532f2fb8645bb389d1ca6ca5a1f55a074
shokishimo <shokishimo@gmail.com>
2023-09-20 22:14:12 -0500

    cdk initial deploy

--- lib/file_link-stack.ts
+++ lib/file_link-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as path from "path";
+
 
 export class FileLinkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaFunc = new lambda.Function(this, "LambdaAPI", {
+      runtime: lambda.Runtime.GO_1_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas')),
+      handler: "main",
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(30),
+      environment: {
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FileLinkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      }
+    });
+
+    lambdaFunc.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE
+    });
   }
 }
