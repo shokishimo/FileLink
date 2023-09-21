2ef3cafc8b2df82b05aef2b439ee55d1d713b7f4
shokishimo <shokishimo@gmail.com>
2023-09-20 23:49:21 -0500

    add s3 bucket to resources

--- lib/file_link-stack.ts
+++ lib/file_link-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import * as path from "path";
 
 
 export class FileLinkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const s3Bucket = new s3.Bucket(this, "S3Bucket", {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.RETAIN
+    });
 
     const lambdaFunc = new lambda.Function(this, "LambdaAPI", {
       runtime: lambda.Runtime.GO_1_X,
@@ -15,9 +21,11 @@ export class FileLinkStack extends cdk.Stack {
       memorySize: 512,
       timeout: cdk.Duration.seconds(30),
       environment: {
-
+        s3Bucket: s3Bucket.bucketName,
       }
     });
+
+    s3Bucket.grantReadWrite(lambdaFunc);
 
     lambdaFunc.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE
