f52842b5a433382549d9e4986fdb3c7b05d15d7e
shokishimo <shokishimo@gmail.com>
2023-09-21 07:02:40 -0500

    add dynamoDB

--- lib/file_link-stack.ts
+++ lib/file_link-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as path from "path";
 
 
@@ -14,18 +15,32 @@ export class FileLinkStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.RETAIN
     });
 
+    const dynamoTable = new dynamodb.Table(this, "DynamoTable", {
+      tableName: "FileLinkDB",
+      partitionKey: { name: "ID", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "Path", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    dynamoTable.addGlobalSecondaryIndex({
+      indexName: "Path-ID-Index",
+      partitionKey: { name: "Path", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "ID", type: dynamodb.AttributeType.STRING },
+    });
+
     const lambdaFunc = new lambda.Function(this, "LambdaAPI", {
       runtime: lambda.Runtime.GO_1_X,
       code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas')),
       handler: "main",
-      memorySize: 512,
-      timeout: cdk.Duration.seconds(30),
+      memorySize: 3008,
+      timeout: cdk.Duration.minutes(15),
       environment: {
         s3Bucket: s3Bucket.bucketName,
       }
     });
 
     s3Bucket.grantReadWrite(lambdaFunc);
+    dynamoTable.grantReadWriteData(lambdaFunc);
 
     lambdaFunc.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE
