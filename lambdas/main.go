df7e79e8f4146ee3ced7d2fe44679df9d845b05e
shokishimo <shokishimo@gmail.com>
2023-09-26 03:35:53 -0500

    api gateway integration

--- lambdas/main.go
+++ lambdas/main.go
@@ -1,36 +1,98 @@
 package main
 
 import (
+	//"context"
 	"log"
-	"context"
+	//"fmt"
+	"net/http"
+	"io"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
-	"github.com/gofiber/fiber/v2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	//"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	//"github.com/gofiber/fiber/v2"
 )
 
-var fiberLambda *fiberadapter.FiberLambda
-
-func init() {
-	log.Printf("Fiber cold start")
-	var app *fiber.App
-	app = fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello UWS!");
-	})
-
-	fiberLambda = fiberadapter.New(app)
+type APIHandler struct {
+	AwsConfig aws.Config
+	DynamoTableName string
+	dbClient  *dynamodb.Client
+	S3BucketName string
+	s3Client *s3.Client
 }
 
-// Handler will deal with Fiber working with Lambda
-func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// If no name is provided in the HTTP request body, throw an error
-	return fiberLambda.ProxyWithContext(ctx, req)
-}
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(Handler)
+	log.Printf("Fiber cold start")
+	// awsConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// apiHandler := APIHandler{
+	// 	AwsConfig: awsConfig,
+	// 	DynamoTableName: "FileLinkDB",
+	// 	dbClient: dynamodb.NewFromConfig(awsConfig),
+	// 	S3BucketName: "file-link-bucket",
+	// 	s3Client: s3.NewFromConfig(awsConfig),
+	// }
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if (r.Method == http.MethodGet) {
+			io.WriteString(w, "root with Get")
+			return
+		}
+		if (r.Method == http.MethodPost) {
+			io.WriteString(w, "root with Post")
+			return
+		}
+	})
+
+	http.HandleFunc("/abc", func(w http.ResponseWriter, r *http.Request) {
+		if (r.Method == http.MethodGet) {
+			io.WriteString(w, "/abc with Get")
+			return
+		}
+		if (r.Method == http.MethodPost) {
+			io.WriteString(w, "/abc with Post")
+			return
+		}
+	})
+
+	// app.Post("/share/:path", func(c *fiber.Ctx) error {
+	// 	givenFile, err := c.FormFile("zip-file")
+	// 	if err != nil {
+	// 		return c.Status(500).SendString(err.Error())
+	// 	}
+
+	// 	// file type check
+
+	// 	// create a read stream to the uploaded file content to grab the contents
+	// 	uploadedFile, err := givenFile.Open()
+	// 	if err != nil {
+	// 		return c.Status(500).SendString("Could not read file")
+	// 	}
+	// 	defer uploadedFile.Close()
+
+	// 	// object to upload
+	// 	objectInput := &s3.PutObjectInput{
+	// 		Bucket: aws.String(apiHandler.S3BucketName),
+	// 		Key:    aws.String(givenFile.Filename),
+	// 		Body:   uploadedFile,
+	// 		ACL:    "public-read",
+	// 	}
+
+	// 	// upload to S3
+	// 	res, err :=apiHandler.s3Client.PutObject(context.TODO(), objectInput);
+	// 	if err != nil {
+	// 		return c.Status(500).SendString(fmt.Sprintf("Failed to upload file to S3: %v", err))
+	// 	}
+
+	// 	// success
+	// 	return c.Status(200).SendString(fmt.Sprintf("Successfully uploaded file to S3: %v", res))
+	// })
+	
+	lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
 }
--- lib/file_link-stack.ts
+++ lib/file_link-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from "path";
-
 
 export class FileLinkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const s3Bucket = new s3.Bucket(this, "S3Bucket", {
+      bucketName: "file-link-s3-bucket",
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.RETAIN
     });
@@ -30,20 +31,36 @@ export class FileLinkStack extends cdk.Stack {
 
     const lambdaFunc = new lambda.Function(this, "LambdaAPI", {
       runtime: lambda.Runtime.GO_1_X,
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas')),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambdas'), {
+        assetHashType: cdk.AssetHashType.OUTPUT,
+        bundling: {
+          image: cdk.DockerImage.fromRegistry('golang:1.19'),
+          command: [
+            'bash',
+            '-c',
+            'GOCACHE=/tmp/go-build GOOS=linux GOARCH=amd64 GOFLAGS=-buildvcs=false go build -o /asset-output/main .',
+          ],
+        },
+      }),
       handler: "main",
       memorySize: 3008,
       timeout: cdk.Duration.minutes(15),
       environment: {
         s3Bucket: s3Bucket.bucketName,
+        dynamoTable: dynamoTable.tableName,
       }
     });
 
     s3Bucket.grantReadWrite(lambdaFunc);
     dynamoTable.grantReadWriteData(lambdaFunc);
-
-    lambdaFunc.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE
-    });
+    
+    const api = new apigateway.LambdaRestApi(this, "apiGateway", {
+      handler: lambdaFunc,
+      proxy: true,
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      }
+    })
   }
 }
