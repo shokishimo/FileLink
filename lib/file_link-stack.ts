dc9e062c8ba189ac08d22dde347b5a16a101612c
shokishimo <shokishimo@gmail.com>
2023-10-11 10:49:17 -0500

    updated s3

--- lib/file_link-stack.ts
+++ lib/file_link-stack.ts
@@ -14,11 +14,18 @@ export class FileLinkStack extends cdk.Stack {
     // S3 Bucket Configuration
     const s3Bucket = new s3.Bucket(this, "S3Bucket", {
       bucketName: "file-link-s3bucket",
-      publicReadAccess: true,
-      // blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      // publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       enforceSSL: true,
       versioned: true, // Ensures new versions of objects are created on overwrite
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.POST, s3.HttpMethods.PUT, s3.HttpMethods.DELETE],
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+        }
+      ]
     });
 
     const dynamoTable = new dynamodb.Table(this, "DynamoTable", {
